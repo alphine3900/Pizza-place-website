f0790d28d4cfb1b32b247ed3a4f45280cd4b1180
alphine3900 <alphine.kirimi@student.moringaschool.com>
2021-11-29 09:40:13 +0300

    add js location details  functinality

--- functioun.js
+++ functioun.js
@@ -8,3 +8,29 @@ function TotalPizza(namePizza,nameCrust,nameTopping){
   
       return this.OwnTopping
   }
+
+  $(document).ready(function() {
+  
+  
+    // Location alert
+        $(".locationOrder").on("click",function(e){
+            e.preventDefault()
+            var name = $("#fname").val();
+            var email = $("#mail").val();
+            var location = $("#txt").val();
+            if (!name || !email || !location) {
+    
+                alert("Fill in all the values")
+    
+            } else {
+              
+                alert(`Your order will be delivered to ${location} `)
+    
+          
+                name = $("#fname").val("");
+                email = $("#mail").val("");
+                location = $("#txt").val("");
+    
+    
+            }
+        })
