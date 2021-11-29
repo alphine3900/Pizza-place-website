32224eb84b0c4413ac064dcb32907fde12f9ec4f
alphine3900 <alphine.kirimi@student.moringaschool.com>
2021-11-29 09:41:40 +0300

    add js selection details  functinality

--- functioun.js
+++ functioun.js
@@ -34,3 +34,37 @@ function TotalPizza(namePizza,nameCrust,nameTopping){
     
             }
         })
+
+         // Customer selection
+  
+      $("#checkbtn").click(function(){
+  
+        var pizzaSize=$("#myPizzaSize option:selected").text() 
+        var pizzaCrust=$("#myPizzaCrust option:selected").text() 
+        var pizzaTopping=$("#myPizzaTopping option:selected").text() 
+ 
+        var pizzaNumbers=parseInt($("#myPizzaSize option:selected").val() )
+        var crustNumbers=parseInt($("#myPizzaCrust option:selected").val() )
+        var toppingNumbers=parseInt($("#myPizzaTopping option:selected").val()) 
+ 
+        var crustInputNumbers=parseInt($("#crustNumber").val()) 
+        var toppingInputNumbers=parseInt($("#toppingNumber").val()) 
+        var pizzaInputNumbers=parseInt($("#pizzaNumber").val()) 
+     //    alert(crustInputNumbers)
+ 
+     var myObject=new TotalPizza(pizzaSize,pizzaCrust,pizzaTopping)
+     var Total=(pizzaNumbers + (crustNumbers*crustInputNumbers)+ (toppingNumbers*toppingInputNumbers))*pizzaInputNumbers;
+ 
+   
+ $(".summary").html("<h2>"+"Pizza Size: "+myObject.OwnPizza+"<br>"+"Crust Type: "+myObject.OwnCrust+"<br>"+"Pizza Topping: "+myObject.myOwnTopping()+"<br>"+"Total Amount:"+"kshs."+Total+"</h2>")
+ 
+ $()
+ 
+     })
+   
+   
+ 
+ })
+          
+
+ 
